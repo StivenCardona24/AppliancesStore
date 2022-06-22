602b1d381b5ba577fea89f67e953d7972087f109
Stiven <stiven.cardona.0924@gmail.com>
2022-06-22 16:38:51 -0500

    Version 3

--- js/main.js
+++ js/main.js
@@ -5,37 +5,124 @@ var app = new Vue({
       {
                 id: 1,
                 img: 'images/aparato-electrico.png',
-                name: 'TV',
-                categorie: 'Televisions',
-                desc: `Tv 4k, smart, LG`,
-                descmodal: ` Tv 4k, smart, LG
-                `,
-                price: 3000000,
+                name: 'SAMSUNG',
+                categorie: 'TV',
+                desc: `Television Samsung 60'' UHD 4k, smartTv`,
+                descmodal: `Television Samsung 60'' UHD 4k, smartTv `,
+                price: 4500000,
                 idm: 1,
                 modal_id: 'pd1',
                 modalw: '#pd1',
                 order_amount: 1,
       },
       {
-        id: 1,
+        id: 2,
         img: 'images/aparato-electrico.png',
-        name: 'TV',
-        categorie: 'Televisions',
-        desc: `Tv 4k, smart, LG`,
-        descmodal: ` Tv 4k, smart, LG
-        `,
-        price: 3000000,
-        idm: 1,
-        modal_id: 'pd1',
-        modalw: '#pd1',
+        name: 'LG',
+        categorie: 'TV',
+        desc: `Television LG 50'' OLED 4k, smartTv, LG`,
+        descmodal: ` Television LG 50'' OLED 4k, smartTv, LG`,
+        price: 3300000,
+        idm: 2,
+        modal_id: 'pd2',
+        modalw: '#pd2',
         order_amount: 1,
-}
+      },
+       {
+                id: 3,
+                img: 'images/aparato-electrico.png',
+                name: 'MABE',
+                categorie: 'FRIDGE',
+                desc: `Fridge MABE No Frost 313 Lts. Brutos RMA300FJCG`,
+                descmodal: `Fridge MABE No Frost 313 Lts. Brutos RMA300FJCG `,
+                price: 1500000,
+                idm: 3,
+                modal_id: 'pd3',
+                modalw: '#pd3',
+                order_amount: 1,
+      },
+      {
+        id: 4,
+        img: 'images/aparato-electrico.png',
+        name: 'SAMSUNG',
+        categorie: 'FRIDGE',
+        desc: `Fridge No Frost SAMSUNG European 307 Lts RB30N4160B`,
+        descmodal: `Fridge No Frost SAMSUNG European 307 Lts RB30N4160B`,
+        price: 2500000,
+        idm: 4,
+        modal_id: 'pd4',
+        modalw: '#pd4',
+        order_amount: 1,
+      },
+      {
+        id: 5,
+        img: 'images/aparato-electrico.png',
+        name: 'IMUSA',
+        categorie: 'FRYER',
+        desc: `Imusa Air Fryer Without Oil Easy Fry 4.2 Liters EZ201D56`,
+        descmodal: `Imusa Air Fryer Without Oil Easy Fry 4.2 Liters EZ201D56 `,
+        price: 440000,
+        idm: 5,
+        modal_id: 'pd5',
+        modalw: '#pd5',
+        order_amount: 1,
+},
+{
+id: 6,
+img: 'images/aparato-electrico.png',
+name: 'OSTER',
+categorie: 'FRYER',
+desc: `OSTER 4.7Lt Digital Air Fryer 2120712`,
+descmodal: `OSTER 4.7Lt Digital Air Fryer 2120712`,
+price: 755000,
+idm: 6,
+modal_id: 'pd6',
+modalw: '#pd6',
+order_amount: 1,
+},
+
+{
+  id: 7,
+  img: 'images/aparato-electrico.png',
+  name: 'WHIRLPOOL',
+  categorie: 'WASHING',
+  desc: `WHIRLPOOL Top Load Washer 19 kg (42 lb) WW19BTAHLA`,
+  descmodal: `WHIRLPOOL Top Load Washer 19 kg (42 lb) WW19BTAHLA`,
+  price: 1940000,
+  idm: 7,
+  modal_id: 'pd7',
+  modalw: '#pd7',
+  order_amount: 1,
+},
+{
+id: 8,
+img: 'images/aparato-electrico.png',
+name: 'LG',
+categorie: 'WASHING',
+desc: `LG Washing Machine Top Load 13 kg (29 lb) WT13BSBP.ABMECOL`,
+descmodal: `LG Washing Machine Top Load 13 kg (29 lb) WT13BSBP.ABMECOL`,
+price: 1790000,
+idm: 8,
+modal_id: 'pd1',
+modalw: '#pd1',
+order_amount: 1,
+},
+
     ],
+
+    products: [],
+  
 
     cart: [],
     totalCart: 0,
     //variables below
     fcartN: '',
+    categorie: "",
+    
+    
+  },
+  mounted(){
+    this.searchFor();
   },
   methods: {
     minusbtn(item){
@@ -93,7 +180,22 @@ var app = new Vue({
           alert('Debe agregar mínimo un producto');
       }
   },
+
+
+  searchFor(){
+
+    if(this.categorie == "all" || this.categorie == ""){
+      
+      this.products = this.appliances;
+    }
+    else{
+      this.products = this.appliances.filter(item => item.categorie == this.categorie);
+     
+    }
+
+    
   },
+  }
 });
 
   //v-for="i in array" es una directiva de bue que permite recorrer un arreglo
