d7f188d9f0a6cf5f9ff92b777b3d39fd10299dd1
Stiven <stiven.cardona.0924@gmail.com>
2022-06-23 00:42:14 -0500

    Version Final

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 var app = new Vue({
   el: "#app",
   data: {
-    appliances:[
+    appliances: [
       {
-                id: 1,
-                img: 'images/tv1.png',
-                name: 'SAMSUNG',
-                category: 'TV',
-                desc: `Television Samsung 60'' UHD 4k, smartTv`,
-                descmodal: `Television Samsung 60'' UHD 4k, smartTv `,
-                price: 4500000,
-                idm: 1,
-                modal_id: 'pd1',
-                modalw: '#pd1',
-                order_amount: 1,
+        id: 1,
+        img: 'images/tv1.png',
+        name: 'SAMSUNG',
+        category: 'TV',
+        desc: `Television Samsung 60'' UHD 4k, smartTv`,
+        descmodal: `Television Samsung 60'' UHD 4k, smartTv `,
+        price: 4500000,
+        idm: 1,
+        modal_id: 'pd1',
+        modalw: '#pd1',
+        order_amount: 1,
       },
       {
         id: 2,
@@ -28,18 +28,18 @@ var app = new Vue({
         modalw: '#pd2',
         order_amount: 1,
       },
-       {
-                id: 3,
-                img: 'images/fridge1.png',
-                name: 'MABE',
-                category: 'FRIDGE',
-                desc: `Fridge MABE No Frost 313 Lts. Brutos RMA300FJCG`,
-                descmodal: `Fridge MABE No Frost 313 Lts. Brutos RMA300FJCG `,
-                price: 1500000,
-                idm: 3,
-                modal_id: 'pd3',
-                modalw: '#pd3',
-                order_amount: 1,
+      {
+        id: 3,
+        img: 'images/fridge1.png',
+        name: 'MABE',
+        category: 'FRIDGE',
+        desc: `Fridge MABE No Frost 313 Lts. Brutos RMA300FJCG`,
+        descmodal: `Fridge MABE No Frost 313 Lts. Brutos RMA300FJCG `,
+        price: 1500000,
+        idm: 3,
+        modal_id: 'pd3',
+        modalw: '#pd3',
+        order_amount: 1,
       },
       {
         id: 4,
@@ -66,140 +66,405 @@ var app = new Vue({
         modal_id: 'pd5',
         modalw: '#pd5',
         order_amount: 1,
-},
-{
-id: 6,
-img: 'images/fryer1.png',
-name: 'OSTER',
-category: 'FRYER',
-desc: `OSTER 4.7Lt Digital Air Fryer 2120712`,
-descmodal: `OSTER 4.7Lt Digital Air Fryer 2120712`,
-price: 755000,
-idm: 6,
-modal_id: 'pd6',
-modalw: '#pd6',
-order_amount: 1,
-},
+      },
+      {
+        id: 6,
+        img: 'images/fryer1.png',
+        name: 'OSTER',
+        category: 'FRYER',
+        desc: `OSTER 4.7Lt Digital Air Fryer 2120712`,
+        descmodal: `OSTER 4.7Lt Digital Air Fryer 2120712`,
+        price: 755000,
+        idm: 6,
+        modal_id: 'pd6',
+        modalw: '#pd6',
+        order_amount: 1,
+      },
 
-{
-  id: 7,
-  img: 'images/lavadora1.png', //'https://exitocol.vtexassets.com/arquivos/ids/12717830-500-auto?v=637873779341170000&width=500&height=auto&aspect=true',
-  name: 'WHIRLPOOL',
-  category: 'WASHING',
-  desc: `WHIRLPOOL Top Load Washer 19 kg (42 lb) WW19BTAHLA`,
-  descmodal: `WHIRLPOOL Top Load Washer 19 kg (42 lb) WW19BTAHLA`,
-  price: 1940000,
-  idm: 7,
-  modal_id: 'pd7',
-  modalw: '#pd7',
-  order_amount: 1,
-},
-{
-id: 8,
-img: 'images/lavadora2.png',
-name: 'LG',
-category: 'WASHING',
-desc: `LG Washing Machine Top Load 13 kg (29 lb) WT13BSBP.ABMECOL`,
-descmodal: `LG Washing Machine Top Load 13 kg (29 lb) WT13BSBP.ABMECOL`,
-price: 1790000,
-idm: 8,
-modal_id: 'pd1',
-modalw: '#pd1',
-order_amount: 1,
-},
+      {
+        id: 7,
+        img: 'images/lavadora1.png', //'https://exitocol.vtexassets.com/arquivos/ids/12717830-500-auto?v=637873779341170000&width=500&height=auto&aspect=true',
+        name: 'WHIRLPOOL',
+        category: 'WASHING',
+        desc: `WHIRLPOOL Top Load Washer 19 kg (42 lb) WW19BTAHLA`,
+        descmodal: `WHIRLPOOL Top Load Washer 19 kg (42 lb) WW19BTAHLA`,
+        price: 1940000,
+        idm: 7,
+        modal_id: 'pd7',
+        modalw: '#pd7',
+        order_amount: 1,
+      },
+      {
+        id: 8,
+        img: 'images/lavadora2.png',
+        name: 'LG',
+        category: 'WASHING',
+        desc: `LG Washing Machine Top Load 13 kg (29 lb) WT13BSBP.ABMECOL`,
+        descmodal: `LG Washing Machine Top Load 13 kg (29 lb) WT13BSBP.ABMECOL`,
+        price: 1790000,
+        idm: 8,
+        modal_id: 'pd1',
+        modalw: '#pd1',
+        order_amount: 1,
+      },
 
     ],
 
     products: [],
-  
 
+    clients: [
+      {
+        id: 1,
+        name: 'Juan Jose',
+        usser: 'Juanjo',
+        password: 1234,
+        cart: [],
+        accounts: [
+          {
+            type: 'Card',
+            name: 'Visa',
+            number: 12345678,
+
+          },
+          {
+            type: 'Api',
+            name: 'Nequi',
+            number: 12345678,
+          }
+        ],
+
+        shopping: [
+
+        ]
+        
+
+      },
+      {
+        id: 2,
+        name: 'Isabela Cardona',
+        usser: 'IsaCar',
+        password: 1234,
+        cart: [],
+        accounts: 
+        [
+          {
+            type: 'Card',
+            name: 'Mastercard',
+            number: 12345678,
+
+          },
+         
+        ],
+        shopping: [
+          
+        ]
+        
+
+      }
+    ],
+
+
+
+    client: '',
     cart: [],
     totalCart: 0,
     //variables below
     fcartN: '',
     category: "categories",
-    
-    
+    modaltrigger: 0,
+    payment: '',
+    usser: "",
+    password: "",
+    logspan: 0,
+    ward: false,
+    nameCard: '',
+    numberCard: '',
+
+
+
   },
-  mounted(){
+  mounted() {
     this.searchFor();
   },
+
   methods: {
-    minusbtn(item){
-      if (item.order_amount > 0) {
-          item.order_amount -= 1;
-      } 
-  },
-  plusbtn(item){
-      if (item.order_amount < 20) {
-          item.order_amount += 1;
-      } 
-  },
-  closemodal(item){
-      item.order_amount = 1;
-  },
-  cartClick(){
-      if (this.cart.length > 0) {
-          const total = this.cart.map(element => element.price * element.qty).reduce((a, b) => a + b, 0);
-          this.totalCart = new Intl.NumberFormat('es-ES', {style: 'currency',currency: 'COP', minimumFractionDigits: 0}).format(total);
+
+
+    login() {
+
+      if (this.usser == "" && this.password == "") {
+        this.logspan = 2;
+        return
       }
-  },
-  addToCart(item){
-      if(item.order_amount > 0 && item.order_amount < 20) {
+
+      this.clients.forEach(element => {
+
+        if (this.usser == element.usser && this.password == element.password) {
+          alert("Bienvenido");
+          this.client = element;
+         
+
+         
           
-          this.cart.push({
-              img: item.img,
-              prod: item.name,
-              qty: item.order_amount,
-              price: item.price,
+          let close = document.getElementById("close");
+          close.click();
+
+
+        }
+
+
+
+      });
+
+      if (this.client == '') {
+        this.logspan = 1;
+      }
+      else{
+       
+        if( this.client.cart.length > 0){
+          this.client.cart.forEach(element => {
+
+            this.addToCart(element);
+            
           });
+        }
 
-          /*acc -> accumulator; cv -> current value*/
-          this.cart = this.cart.reduce((acc, cv) => {
-              const elementExists = acc.find(e => e.prod === cv.prod);
-              if (elementExists) {
-              return acc.map((e) => {
-                  if (e.prod === cv.prod) {
-                  return {
-                      ...e,
-                      qty: e.qty + cv.qty
-                  }
-                  }
-                  return e;
-              });
-              }
-              return [...acc, cv];
-          }, []);
+          this.client.cart = this.cart.map(p => p);
 
-          this.fcartN = this.cart.length;
+          
 
-          alert(`Se agregaron ${item.order_amount} ${item.name} al carrito`);
-          item.order_amount = 1;
 
-      }else{
-          alert('Debe agregar mínimo un producto');
+        
+        
+    }
+
+    },
+    logout(){
+      if (confirm("¿Are you sure to log out?") === true){
+          this.modaltrigger = 0;
+          this.logspan = 0;
+          this.usser = '';
+          this.password = '';
+          this.client = '';
+          this.cancel();
       }
   },
 
+  closelogin(){
+    this.logspan = 0;
+    this.usser = '';
+    this.password = '';
+},
 
-  searchFor(){
+    minusbtn(item) {
+      if (item.order_amount > 0) {
+        item.order_amount -= 1;
+      }
+    },
+    plusbtn(item) {
+      if (item.order_amount < 20) {
+        item.order_amount += 1;
+      }
+    },
 
-    if(this.category == "all" || this.category == "categories"){
+    closemodal(item) {
+      item.order_amount = 1;
+    },
+    cartClick() {
+      if (this.cart.length > 0) {
+        const total = this.cart.map(element => element.price * element.order_amount).reduce((a, b) => a + b, 0);
+        this.totalCart = new Intl.NumberFormat('es-ES', { style: 'currency', currency: 'COP', minimumFractionDigits: 0 }).format(total);
+      }
+    },
+
+    addToCart(item) {
+      if (item.order_amount > 0 && item.order_amount < 20 ) {
+
+        this.cart.push({
+          img: item.img,
+          name: item.name,
+          order_amount: item.order_amount,
+          price: item.price,
+        });
+
+        /*acc -> accumulator; cv -> current value*/
+        this.cart = this.cart.reduce((acc, cv) => {
+          const elementExists = acc.find(e => e.name === cv.name);
+          if (elementExists) {
+            return acc.map((e) => {
+              if (e.name === cv.name) {
+                return {
+                  ...e,
+                  order_amount: e.order_amount + cv.order_amount
+                }
+              }
+              return e;
+            });
+          }
+          return [...acc, cv];
+        }, []);
+
+        this.fcartN = this.cart.length;
+
+        alert(` ${item.order_amount} ${item.name} added to cart`);
+        item.order_amount = 1;
+
+        if(this.client != ''){
+          this.client.cart = this.cart.map((p) => p);
+        }
+
+      } else {
+        alert('you must add at least one product');
+      }
+    },
+    delFromCart(index) {
+      this.cart.splice(index, 1);
+      const total = this.cart.map(element => element.price * element.order_amount).reduce((a, b) => a + b, 0);
+      this.totalCart = new Intl.NumberFormat('es-ES', { style: 'currency', currency: 'COP', minimumFractionDigits: 0 }).format(total);
+      if (this.cart.length > 0) {
+        this.fcartN = this.cart.length;
+      } else {
+        this.fcartN = '';
+      }
+    },
+
+
+    cancel() {
+      if (this.cart.length > 0) {
+        this.cart.splice(0, this.cart.length);
+        const total = this.cart.map(element => element.price * element.order_amount).reduce((a, b) => a + b, 0);
+        this.totalCart = new Intl.NumberFormat('es-ES', { style: 'currency', currency: 'COP', minimumFractionDigits: 0 }).format(total);
+        //alert('Su pedido fue cancelado satisfactoriamente');
+        this.appliances.forEach(element => element.order_amount = 1);
+       
+        this.fcartN = '';
+      } else {
+        const total = this.cart.map(element => element.price * element.order_amount).reduce((a, b) => a + b, 0);
+        this.totalCart = new Intl.NumberFormat('es-ES', { style: 'currency', currency: 'COP', minimumFractionDigits: 0 }).format(total);
+      }
+
       
-      this.products = this.appliances;
-    }
-    else{
-      this.products = this.appliances.filter(item => item.category == this.category);
-      
+    },
+
+    searchFor() {
+
+      if (this.category == "all" || this.category == "categories") {
+
+        this.products = this.appliances;
+      }
+      else {
+        this.products = this.appliances.filter(item => item.category == this.category);
+
+
+      }
+
+      window.location.href = "#products"
+
+
+
+
+    },
+
+    pay(){
+
+      if(this.cart.length == 0){
+          alert("No tienes productos añadidos al carrito");
+          return
+      }
+
+      if(this.client == ''){
+        alert("You must log in")
+        let log = document.getElementById("log");
+        log.click();
+      }
+
+      this.ward = true;
+
+
      
+
+      
+
+
+  },
+
+    buy() {
+      if(this.payment == ''){
+        alert("Enter the payment method");
+        return
+       }
+
+       this.client.accounts.forEach(element => {
+        if(this.payment == element.type){
+          alert("your purchase has been made successfully");
+          this.client.shopping.push(this.cart.map(e => e));
+          this.ward = false;
+          this.payment = '';
+          this.cancel();
+          let close = document.getElementById("close");
+          close.click();
+          return
+         }
+        
+       });
+
+       if(this.cart.length > 0){
+        alert("You do not have this purchase method available");
+        let log = document.getElementById("add");
+        log.click();
+
+
+       }
+     
+       this.ward = false;
+      
+
+      
+
+       
+    },
+
+    save(){
+
+      let ward = true;
+      if(this.payment != '' && this.nameCard != '' && this.numberCard != ''){
+
+        this.client.accounts.forEach(element => {
+
+          if(this.nameCard == element.name && this.numberCard == element.number){
+            alert("You already have an account");
+            ward = false;
+
+          }
+          
+        });
+
+        if(ward){
+          this.client.accounts.push({
+            type: this.payment,
+            name: this.nameCard,
+            number: this.numberCard
+          })
+
+          alert("account added successfully");
+          let close = document.getElementById("close");
+          close.click();
+        }
+
+
+
+      }
+      else{
+        alert("Enter the data correctly");
+      }
+
+      this.nameCard = '';
+      this.numberCard = '';
+
+
     }
 
-    window.location.href = "#products"
 
-    
-
-    
-  },
   }
 });
 
